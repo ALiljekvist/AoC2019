0fc4dc6011ef29132e2250d3611d7cd994ea7c30
Anton Liljekvist <jantonalmqvist@gmail.com>
2025-12-05 22:03:55 +0100

    Update intcode solution to make it easier to work with

--- day2/day2.py
+++ day2/day2.py
@@ -1,34 +1,38 @@
-def readInput():
-    data = open('input.txt', 'r').read()
-    program = list(map(lambda x: int(x), data.split(',')))
-    return program
+def read_input(filename):
+    with open(filename, 'r') as f:
+        return [int(x) for x in f.read().split(',')]
 
-def intcodeComputer(program):
+def run(program):
     ind = 0
     while program[ind] != 99:
-        if program[ind] != 1 and program[ind] != 2:
-            return program
         in1, in2, out = program[ind+1], program[ind+2], program[ind+3]
-        if program[ind] == 1:
-            program[out] = program[in1] + program[in2]
-        elif program[ind] == 2:
-            program[out] = program[in1] * program[in2]
+        match program[ind]:
+            case 1:
+                program[out] = program[in1] + program[in2]
+            case 2:
+                program[out] = program[in1] * program[in2]
+            case _:
+                return program
         ind += 4
     return program
 
-def setProgram(program, noun, verb):
-    newprogram = program.copy()
-    newprogram[1] = noun
-    newprogram[2] = verb
-    return newprogram
+def part1(program):
+    program[1] = 12
+    program[2] = 2
+    result = run(program)
+    return result[0]
 
-if __name__ == '__main__':
-    program = readInput()
+def part2(program):
     for i in range(100):
         for j in range(100):
-            newprogram = setProgram(program, i, j)
-            newprogram = intcodeComputer(newprogram)
-            if i == 12 and j == 2:
-                print('Answer for part 1:', newprogram[0])
-            if newprogram[0] == 19690720:
-                print('Answer for part 2:', 100*i + j)
+            test_program = program.copy()
+            test_program[1] = i
+            test_program[2] = j
+            if run(test_program)[0] == 19690720:
+                return 100*i + j
+    return -1
+
+if __name__ == '__main__':
+    program = read_input('input.txt')
+    print(part1(program.copy()))
+    print(part2(program))
