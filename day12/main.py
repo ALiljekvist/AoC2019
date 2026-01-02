ae30a8e9177a3ce7549ba735becc3b97bf7e93eb
Anton Liljekvist <jantonalmqvist@gmail.com>
2026-01-02 19:54:07 +0100

    Full solution for day 12
    I knew that it would be some preiodic pattern after I implemented
    part 1, but did not have the energy to look for the pattern at
    that time.

--- day12/main.py
+++ day12/main.py
@@ -1,3 +1,5 @@
+import math
+
 class Moon:
     def __init__(self, raw_pos: str):
         self.pos = [int(y.split('=')[1]) for y in raw_pos[1:-1].split(', ')]
@@ -31,31 +33,49 @@ def read_input(filename):
     except:
         return None
 
-def simulate(moons, timesteps, original_positions=None):
+def search_for_period(x, win_size=100):
+    period = None
+    if len(x) < win_size+1:
+        return None
+    for i in range(win_size):
+        if x[i] != x[len(x)-win_size+i]:
+            return None
+    return len(x)-win_size
+
+def simulate(moons, timesteps):
+    speed_hists = [[], [], []]
+    periods = [None, None, None]
     for t in range(timesteps):
         for i in range(len(moons)):
             for j in range(i+1, len(moons)):
                 moons[i].apply_gravity(moons[j])
         for i, m in enumerate(moons):
             m.move()
-            if original_positions is None:
+        for i in range(len(periods)):
+            period = periods[i]
+            if period is not None:
                 continue
-            if all([m.pos[j] == original_positions[i][j] for j in range(len(m.pos))]):
-                print(f"Moon {i+1} repeated position after {t} steps")
-        print(t, moons[0])
-    return moons
+            if period is None:
+                speed_hists[i].append(moons[0].pos[i])
+                period = search_for_period(speed_hists[i])
+            if period is not None:
+                periods[i] = period
+        if all([p is not None for p in periods]):
+            break
+    return moons, periods
 
 def part1(data):
-    moons = simulate(data, 1000)
+    moons, _ = simulate(data, 1000)
     return sum([m.energy() for m in moons])
 
 def part2(data):
-    original_positions = [m.pos[:] for m in data]
-    moons = simulate(data, 1000000, original_positions)
-    return moons
+    _, periods = simulate(data, 10000000000)
+    lcm = math.lcm(periods[0], periods[1])
+    lcm = math.lcm(lcm, periods[2])
+    return lcm
 
 if __name__ == "__main__":
     data = read_input('input.txt')
     # data = read_input('example.txt')
-    print(part1(data))
+    print(part1(data.copy()))
     print(part2(data))
