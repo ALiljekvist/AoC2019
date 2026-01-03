8c968be6b8f3c3b6da796767f2dc381b7c123adb
Anton Liljekvist <jantonalmqvist@gmail.com>
2026-01-03 16:33:51 +0100

    Full solution for day 22
    Had to rethink my approach entierly. I wanted to do an inverse
    shuffle function at first and iterate that, but I did not see how
    to make it effectively. I did look in the reddit threads and saw
    the tip to treat the entire shuffle as a linear function and to
    break the input number into a linear combination as well. This
    helped a lot in breaking the problem down.

--- day22/main.py
+++ day22/main.py
@@ -6,74 +6,65 @@ def read_input(filename):
     except:
         return None
 
-def new_stack(deck):
-    return list(reversed(deck))
-
-def deal(deck, increment):
-    new_deck = [0 for x in range(len(deck))]
-    ind = 0
-    while deck:
-        new_deck[ind] = deck.pop(0)
-        ind = (ind+increment) % len(new_deck)
-    return new_deck
-
-def cut(deck, at):
-    if at < 0:
-        at += len(deck)
-    return deck[at:] + deck[:at]
-
-def shuffle(operations, deck_size=10007):
-    deck = list(range(deck_size))
+def shuffle(operations, a, b, N):
     for op in operations:
         things = op.split(' ')
         if things[0] == 'cut':
-            deck = cut(deck, int(things[-1]))
+            fa, fb = 1, -int(things[-1])
         if things[0] == 'deal':
             try:
-                increment = int(things[-1])
-                deck = deal(deck, increment)
+                # deal with increment
+                fa, fb, = int(things[-1]), 0
             except:
-                deck = new_stack(deck)
-    return deck
+                # deal into new stack
+                fa, fb = -1, -1
+        # Since these can be written as linear functions, we can write the
+        # mappings as a F(a) on the unit (1) and an offset F(b).
+        # Each new mapping then becomes:
+        # F(a) * (a * x + b) + F(b) = F(a)*a*x + F(a)*b + F(b)
+        # Since this in turn is linear, the x can be multiplied later
+        # Use the modulo operator to not have the numbers go off far.
+        # Since the final answer is also mod N, this will not affect any
+        # intermediate answer or the final answer.
+        a = (fa * a) % N
+        b = (fa * b + fb) % N
+    return a, b
 
-def run_tests():
-    tests = [(["deal with increment 7",
-              "deal into new stack",
-              "deal into new stack"],
-              [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]),
-              (["cut 6",
-              "deal with increment 7",
-              "deal into new stack"],
-              [3, 0, 7, 4, 1, 8, 5, 2, 9, 6]),
-              (["deal with increment 7",
-              "deal with increment 9",
-              "cut -2"],
-              [6, 3, 0, 7, 4, 1, 8, 5, 2, 9]),
-              (["deal into new stack",
-              "cut -2",
-              "deal with increment 7",
-              "cut 8",
-              "cut -4",
-              "deal with increment 7",
-              "cut 3",
-              "deal with increment 9",
-              "deal with increment 3",
-              "cut -1",],
-              [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]),]
-    for i, (ops, res) in enumerate(tests):
-        out = shuffle(ops, deck_size=10)
-        if len(out) != len(res):
-            print("Test", i+1, "output len mismatch", len(out), len(res))
-            continue
-        matching = True
-        for j in range(len(out)):
-            if out[j] != res[j]:
-                matching = False
-        if not matching:
-            print("Fail", i+1, out, res)
+def part1(data):
+    N = 10007
+    # Represent the number c as c = (a * x + b), and the function in modular form will become
+    # F(c) mod N = (F(a) * x + F(b)) mod N
+    a, b = shuffle(data, 1, 0, N)
+    return (a * 2019 + b) % N
+
+# This uses Fermat's little theorem to calculate a modular inverse
+# of a mod N (this is given that N is a prime number)
+def inv(a, N):
+    return pow(a, N-2, N)
+
+def part2(data):
+    N = 119315717514047
+    M = 101741582076661
+    a, b = shuffle(data, 1, 0, N)
+    # The shuffle is now to be performed M times, where the entire shuffle can be written as it's own linear function:
+    # a' = F(a) * a'
+    # b' = F(a) * b + F(b)
+    # The F(a) is now 'a' and F(b) is 'b', and we start from a = 1 and b = 0 again.
+    # This done M times can be simplified to become:
+    # F(a) = a^M mod N = a^M, mod N
+    # F(b) = (a * b + F(b))^M, mod N
+    #      = b * (a^M + a^(M-1) + ... + a^2 + a + 1), mod N
+    #      = b * (a^M - 1) * inv(a-1), mod N
+    a_M = pow(a, M, N)
+    b_M = b * (a_M - 1) * inv(a-1, N)
+
+    # The final answer can then be found through the linear fucntion as in part 1.
+    # However, we now want to find the original position so we have to solve for x:
+    # F(c) = F(a)*x + F(b) mod N  =>
+    # x = (F(c) - F(b)) * inv(F(a)) mod N
+    return ((2020 - b_M) * inv(a_M, N)) % N
 
 if __name__ == '__main__':
     data = read_input('input.txt')
-    run_tests()
-    out = shuffle(data)
-    print(out.index(2019))
+    print(part1(data))
+    print(part2(data))
