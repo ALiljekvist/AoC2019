143df204ff94743d3b098296bb90fe8c32b22a5d
Anton Liljekvist <jantonalmqvist@gmail.com>
2026-01-05 00:45:34 +0100

    Full solution for day 16
    That completes all my stars. That was a fun year.

--- day16/main.py
+++ day16/main.py
@@ -12,25 +12,38 @@ def perform_fft(numbers):
     for i in range(len(numbers)):
         part_sum = 0
         pos = 0
-        # print('---', i, '---')
         for j in range(i, len(numbers)):
             if (j - i) % (i+1) == 0 and j != i:
                 pos = (pos+1) % len(pattern)
-            # print(numbers[j], pattern[pos])
             part_sum += numbers[j] * pattern[pos]
         if part_sum < 0:
             part_sum = -part_sum
         numbers[i] = part_sum % 10
-        # print(part_sum, numbers[i])
     return numbers
 
 def part1(data, num_ffts=100):
     for i in range(num_ffts):
-        # print(i, data)
         data = perform_fft(data)
-    return data
+    return ''.join([str(x) for x in data[:8]])
+
+def part2(data):
+    offset = sum([int(v) * 10**i for i, v in enumerate(reversed(data[:7]))])
+    new_data = data * 10000
+    new_data = new_data[offset:]
+    # By printing the length of the real input signal and the offset, I quickly
+    # realized that the pattern was the same for all the remaining numbers
+    # after the offset and that I could compute a running summation
+    # backwards instead.
+    for _ in range(100):
+        rem = 0
+        for i in reversed(range(len(new_data))):
+            rem += new_data[i]
+            rem = rem % 10
+            new_data[i] = rem
+    return ''.join([str(s) for s in new_data[:8]])
 
 if __name__ == '__main__':
     data = read_input('input.txt')
     # data = read_input('example.txt')
-    print(''.join([str(x) for x in part1(data, num_ffts=100)[:8]]))
+    print(part1(data.copy()))
+    print(part2(data))
